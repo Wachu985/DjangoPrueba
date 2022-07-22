2433be2cca4a2fa164eb64c2484b41ab8ef5368e
Wachu985 <93799115+Wachu985@users.noreply.github.com>
2022-07-22 01:55:37 -0400

    Create Prod

---
+++ project01/settings/prod.py
@@ -0,0 +1,31 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'empleados',
+        'USER':'postgres',
+        'PASSWORD':'caperia985',
+        'HOST':'127.0.0.1',
+        'PORT':'5432',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR/'static/']
+
+"""Configurando la URL con que van a salir los Archivos Media"""
+MEDIA_URL = '/media/'
+"""Configurando la direccion de la Carpeta Media"""
+MEDIA_ROOT = BASE_DIR
